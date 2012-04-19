400679d07707d58f010e8798a55bbe8754605789
Evaristo Gestoso Rodriguez <evaristogestosorodrigez@gmail.com>
2012-04-19 23:08:17 +0200

    Subo la estructura del proyecto al repositorio de GitHub

---
+++ OSRM.config.js
@@ -0,0 +1,36 @@
+/*
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU AFFERO General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+or see http://www.gnu.org/licenses/agpl.txt.
+*/
+
+// OSRM config file
+// [has to be loaded directly after OSRM.base]
+
+OSRM.DEFAULTS = {
+	HOST_ROUTING_URL: 'http://router.project-osrm.org/viaroute',
+	HOST_SHORTENER_URL: 'http://map.project-osrm.org/shorten/',
+	HOST_GEOCODER_URL: 'http://nominatim.openstreetmap.org/search',
+	HOST_REVERSE_GEOCODER_URL: 'http://nominatim.openstreetmap.org/reverse',
+	WEBSITE_URL: document.URL.replace(/#*(\?.*|$)/i,""),					// truncates URL before first ?, and removes tailing #
+	JSONP_TIMEOUT: 5000,
+	ZOOM_LEVEL: 14,
+	ONLOAD_LATITUDE: 42,
+	ONLOAD_LONGITUDE: -6,
+	ONLOAD_SOURCE: "",
+	ONLOAD_TARGET: "",
+	HIGHLIGHT_ZOOM_LEVEL: 16,
+	LANGUAGE: "es",
+	GEOCODER_BOUNDS: '&bounded=1&viewbox=-27.0,72.0,46.0,36.0'
+};
